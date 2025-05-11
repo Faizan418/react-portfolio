6cfd1bebdc2852355aad0552104fa9f5a334b419
Faizan418 <muhammadf4060@gmail.com>
2025-05-12 02:21:42 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Navbar from './components/Navbar';
      function App() {
        return (
          <div className="container grid gap-3 max-w-7xl mx-auto px-3 sm:px-4 lg:px-6 py-6 lg:grid-cols-[80px_1fr_2fr] md:grid-cols-[80px_1fr] grid-cols-1">
+          
            <Navbar />
            <Profile />
            <div className="box content p-3 sm:p-4 lg:p-5 rounded-2xl bg-[var(--accent-03)] md:col-span-2 col-span-1">
@@ -19,6 +20,7 @@ import Navbar from './components/Navbar';
              <Contributions />
              <Projects />
              <Contact />
+
            </div>
          </div>
        );
